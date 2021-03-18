a69eb1fb75ffb9fe9d0fd5bff049e2dd8584e060
kdiao2 <77560527+kdiao2@users.noreply.github.com>
2021-03-18 18:25:41 -0500

    Added RestaurantInterface File

---
+++ RestaurantInterface.java
@@ -0,0 +1,18 @@
+// --== CS400 File Header Information ==--
+// Name: <Kenneth Diao>
+// Email: <kdiao2@wisc.edu>
+// Team: <Red>
+// Group: <KF>
+// TA: <Keren Chen>
+// Lecturer: <Gary Dahl>
+// Notes to Grader: none
+
+public interface RestaurantInterface extends Comparable<RestaurantInterface> {
+	public Integer getRank();
+	public String getRestaurantName();
+	public Integer getNumSales();
+	public String getCity();
+	public String getState();
+	public Double getAvgCheck();
+	public Integer numMealsServed();
+}
